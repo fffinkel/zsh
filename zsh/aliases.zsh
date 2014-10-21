1e64749c1883728a52942f580659256b53a4ecde
Matt Finkel <finkel.matt@gmail.com>
2014-10-21 10:39:09 -0500

    Zsh alias updates
    Add function to check out ticket from upstream based on jira ticket name

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,9 +3,33 @@ alias gst='git status'
 alias gl='git log --graph --pretty=oneline'
 alias glo='git log --graph'
 alias glog='git log -p --graph'
+alias glos='git log --stat'
 
 function logsubsearch() { git log -L '/sub '"$1"' {/',/^}/:$2 }
 alias gls=logsubsearch
+
+alias gb=new_local_ticket
+function new_local_ticket() {
+	PROJECT="CIR"
+	ticket="$PROJECT\-$1"
+	count=0
+
+	git fetch
+
+	for i in $(git branch -r)
+		do
+			if [[ $i =~ $ticket ]]; then
+				branch=$(sed -e's/\s*origin\///' <<< $i)
+				count=$((count + 1))
+			fi
+	done
+
+	if [[ $count -ne 1 ]]; then
+		echo "'$ticket' matches $count branches, exiting"
+	else
+		git checkout -b $branch origin/$branch
+	fi
+}
 
 alias gd='git diff'
 alias gdc='git diff --cached'
@@ -13,8 +37,7 @@ alias gdc='git diff --cached'
 alias ga='git add'
 alias gci='git commit --verbose'
 
-alias gb='git branch -vv'
-alias gco='git checkout -b'
+alias gco='git checkout '
 
 alias gf='git fetch'
 alias gp='git pull'
