4f81310491a3dbab0d4f9d909e44450c41f5c23b
Matt Finkel <mattf@ziprecruiter.com>
2019-02-22 12:22:00 -0800

    Update prompt
    Properly hide the branch name if we're not in a repo. Also, update the
    zsh-git-prompt submodule to fix a bug.

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,3 +1,4 @@
+
 source ~/.zsh/git-prompt/zshrc.sh
 
 ZSH_THEME_GIT_PROMPT_PREFIX=" "
@@ -5,6 +6,17 @@ ZSH_THEME_GIT_PROMPT_SUFFIX=""
 ZSH_THEME_GIT_PROMPT_SEPARATOR=""
 ZSH_THEME_GIT_PROMPT_BRANCH=""
 
+__prompt () {
+
 PROMPT="
-%{$fg[yellow]%}%n%{$reset_color%} at %{$fg[yellow]%}%m%{$reset_color%} in %{$fg[yellow]%}%~%{$reset_color%} on%{$fg[yellow]%}\$(git_super_status)%{$reset_color%}
+%{$fg[yellow]%}%n%{$reset_color%} at %{$fg[yellow]%}%m%{$reset_color%} in %{$fg[yellow]%}%~%{$reset_color%}"
+
+IN_A_REPO_BABY=`git rev-parse --is-inside-work-tree 2>/dev/null`
+[ $IN_A_REPO_BABY ] && PROMPT+=' on%{$fg[yellow]%}`git_super_status`%{$reset_color%}'
+
+PROMPT+="
 [%w %t] %% "
+
+}
+
+precmd() { __prompt; }
