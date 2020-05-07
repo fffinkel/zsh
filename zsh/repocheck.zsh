598819652a1a830b0d123dcc9b9ab53d99a42bf4
Matt Finkel <mattf@ziprecruiter.com>
2020-05-06 19:02:59 -0700

    Update prefix file deletion
    Delete this file only if there are no warnings. This makes current
    warnings appear immediately on new prompts.

--- zsh/repocheck.zsh
+++ zsh/repocheck.zsh
@@ -5,10 +5,6 @@ CONFIGS_DIR="$HOME/cnf"
 OOD_WARNING="WARNING: %s dir is out of date"
 WTU_WARNING="WARNING: %s dir has uncommitted changes"
 
-if [[ -a $PREFIX_FILE ]] then
-  rm $PREFIX_FILE
-fi
-
 for REPO in $(ls $CONFIGS_DIR); do
 
   REPO_DIR=$CONFIGS_DIR/$REPO
@@ -16,11 +12,17 @@ for REPO in $(ls $CONFIGS_DIR); do
   REPO_STATUS=$(git --git-dir=$REPO_DIR/.git status)
 
   if [[ "$REPO_STATUS" == *"Your branch is behind 'origin/master'"* ]]; then
-    printf "$OOD_WARNING\n" $REPO >> $PREFIX_FILE
+    printf "$OOD_WARNING\n" $REPO >> $PREFIX_STRING
   fi
 
   if [[ "$REPO_STATUS" != *"working tree clean"* ]]; then
-    printf "$WTU_WARNING\n" $REPO >> $PREFIX_FILE
+    printf "$WTU_WARNING\n" $REPO >> $PREFIX_STRING
   fi
+
+  if [[ $PREFIX_STRING == "" ]]; then
+    rm $PREFIX_FILE
+  else
+    echo $PREFIX_STRING > $PREFIX_FILE
+  done
 
 done
