8c43bfcdc66f9e00f353cabcd8dd08fe1bc6d927
Matt Finkel <mattf@ziprecruiter.com>
2020-05-06 19:07:17 -0700

    Bugfix repo check

--- zsh/repocheck.zsh
+++ zsh/repocheck.zsh
@@ -8,21 +8,21 @@ WTU_WARNING="WARNING: %s dir has uncommitted changes"
 for REPO in $(ls $CONFIGS_DIR); do
 
   REPO_DIR=$CONFIGS_DIR/$REPO
-  git --git-dir=$REPO_DIR/.git fetch > /dev/null
-  REPO_STATUS=$(git --git-dir=$REPO_DIR/.git status)
+  git --git-dir=$REPO_DIR/.git --work-tree=$REPO_DIR fetch > /dev/null
+  REPO_STATUS=$(git --git-dir=$REPO_DIR/.git --work-tree=$REPO_DIR status)
 
   if [[ "$REPO_STATUS" == *"Your branch is behind 'origin/master'"* ]]; then
-    printf "$OOD_WARNING\n" $REPO >> $PREFIX_STRING
+    PREFIX_STRING+="$(printf "$OOD_WARNING" $REPO)\n"
   fi
 
   if [[ "$REPO_STATUS" != *"working tree clean"* ]]; then
-    printf "$WTU_WARNING\n" $REPO >> $PREFIX_STRING
+    PREFIX_STRING+="$(printf "$WTU_WARNING" $REPO)\n"
   fi
 
-  if [[ $PREFIX_STRING == "" ]]; then
-    rm $PREFIX_FILE
-  else
-    echo $PREFIX_STRING > $PREFIX_FILE
-  done
-
 done
+
+if [[ $PREFIX_STRING == "" ]]; then
+  rm $PREFIX_FILE
+else
+  echo $PREFIX_STRING > $PREFIX_FILE
+fi
