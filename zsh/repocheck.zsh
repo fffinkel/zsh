b29058c529356390711128033a407aae95d9b3e7
Matt Finkel <mattf@ziprecruiter.com>
2020-10-21 13:27:14 -0500

    Bugfix repocheck

--- zsh/repocheck.zsh
+++ zsh/repocheck.zsh
@@ -2,18 +2,12 @@
 
 PREFIX_FILE="/tmp/.prompt_prefix"
 CONFIGS_DIR="$HOME/cnf"
-OOD_WARNING="WARNING: %s dir is out of date"
-WTU_WARNING="WARNING: %s dir has uncommitted changes"
+OOD_WARNING="WARNING: %s is out of date"
+WTU_WARNING="WARNING: %s has uncommitted changes"
 
 check_dir() {
   for REPO in $(ls $CONFIGS_DIR); do
-
     DIR=$CONFIGS_DIR/$REPO
-    if [[ -d $DIR ]]; then
-      cd $DIR
-      check_dir
-    fi
-
     if [[ ! -d $DIR/.git ]]; then
       continue
     fi
@@ -22,15 +16,17 @@ check_dir() {
     REPO_STATUS=$(git --git-dir=$DIR/.git --work-tree=$DIR status)
 
     if [[ "$REPO_STATUS" == *"Your branch is behind 'origin/master'"* ]]; then
-      PREFIX_STRING+="$(printf "$OOD_WARNING" $REPO)\n"
+      PREFIX_STRING+="$(printf "$OOD_WARNING" $DIR)\n"
     fi
 
     if [[ "$REPO_STATUS" != *"working tree clean"* ]]; then
-      PREFIX_STRING+="$(printf "$WTU_WARNING" $REPO)\n"
+      PREFIX_STRING+="$(printf "$WTU_WARNING" $DIR)\n"
     fi
 
   done
 }
+
+check_dir
 
 if [[ $PREFIX_STRING == "" ]]; then
   if [[ -a $PREFIX_FILE ]]; then
