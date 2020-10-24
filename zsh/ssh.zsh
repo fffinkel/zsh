d9ac8d16c07863533d124b2dd734c55319ef3848
Matt Finkel <finkel.matt@gmail.com>
2020-10-24 19:35:29 +0000

    Fix up ssh init, only use keychain on macos

--- zsh/ssh.zsh
+++ zsh/ssh.zsh
@@ -1,13 +1,17 @@
 
 RUNCOUNT=$(ps -ef | grep "ssh-agent" | grep -v "grep" | wc -l)
 if [ $RUNCOUNT -eq 0 ]; then
-  echo Starting SSH Agent
-  eval $(ssh-agent -s)
+  echo Starting SSH Agent;
+  eval $(ssh-agent -s);
 else
-  echo Found SSH Agent
+  echo Found SSH Agent;
 fi
 
-ssh-add -K ~/.ssh/id_rsa
+if [ $OSTYPE = darwin ]; then
+  ssh-add -K;
+else
+  ssh-add;
+fi
 
 if [[ -S "$SSH_AUTH_SOCK" && ! -h "$SSH_AUTH_SOCK" ]]; then
     ln -sf "$SSH_AUTH_SOCK" ~/.ssh/auth.sock;
